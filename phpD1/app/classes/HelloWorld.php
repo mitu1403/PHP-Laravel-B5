bd809e72eb3c474bb92920e452ffe98642e52f30
Mahmuda Akter <mmitu163069@bscse.uiu.ac.bd>
2022-05-18 02:05:46 +0600

    phpD1

---
+++ phpD1/app/classes/HelloWorld.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\classes;
+
+
+class HelloWorld
+{
+    public $message;
+
+    public function __construct()
+    {
+        $this->message = 'hello World';
+    }
+    public function index()
+    {
+        echo $this->message;
+    }
+}
